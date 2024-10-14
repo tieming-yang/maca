b866def3dcc04ff2b450a06085d81d6cf331379c
Snow Yáng <snow.yang@fluv.com>
2024-10-14 20:03:03 +0800

    Init project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,90 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import Youtube from "react-youtube";
+import { lyrics } from "./osaka-lover";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [player, setPlayer] = useState(null);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+  const [currentTimeStamp, setCurrentTimeStamp] = useState(0);
+  const [currentLine, setCurrentLine] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  useEffect(() => {
+    const currentLyric = lyrics.find((lyric, index) => {
+      const nextLyric = lyrics[index + 1];
+      return (
+        currentTimeStamp >= lyric.timeStamp &&
+        (!nextLyric || currentTimeStamp < nextLyric.timeStamp)
+      );
+    });
+
+    if (currentLyric) {
+      setCurrentLine(currentLyric.lyric);
+    }
+  }, [currentTimeStamp]);
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Youtube
+        // @ts-expect-error as I dont want to handle Youtube player event
+        onReady={(e) => {
+          setPlayer(e.target);
+        }}
+        onPlay={() => {
+          const id = setInterval(() => {
+            // @ts-expect-error as I dont want to handle Youtube player event
+            setCurrentTimeStamp(player?.getCurrentTime());
+          }, 100);
+
+          setIntervalId(id);
+        }}
+        onPause={() => {
+          if (!intervalId) {
+            console.warn("intervalId is null");
+            return;
+          }
+
+          clearInterval(intervalId);
+        }}
+        onEnd={() => {
+          if (!intervalId) {
+            console.warn("intervalId is null");
+            return;
+          }
+
+          clearInterval(intervalId);
+          setCurrentTimeStamp(0);
+        }}
+        videoId="E-DAUGDEeRA"
+        opts={opts}
+      />
+
+      <ul>
+        {lyrics.map((lyric) => {
+          return (
+            <li
+              className={`text-center leading-10 transition-all duration-300 ${
+                currentLine === lyric.lyric
+                  ? "text-white font-bold text-3xl"
+                  : "text-white/70"
+              }`}
+              key={lyric.timeStamp}
+            >
+              {lyric.lyric}
+            </li>
+          );
+        })}
+      </ul>
+    </main>
   );
 }
