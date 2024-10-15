cfb2b53d06ef3a2d0a4aa471bf7bf10c37e416a1
Snow Yáng <snow.yang@fluv.com>
2024-10-15 20:49:21 +0800

    Add contact

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Link className="fixed top-3 left-7" href="https://github.com/snow-t-m-yang" target="_blank">
+          @kurogane
+        </Link>
         {children}
       </body>
     </html>
