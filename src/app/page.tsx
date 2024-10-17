9175515c17c385370921f9722fb218dd59f4b79d
Snow Yáng <snow.yang@fluv.com>
2024-10-17 20:05:27 +0800

    Add player control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useRef, useState } from "react";
 import Youtube from "react-youtube";
 import { Line, lyric } from "./osaka-lover";
+import { PlayIcon, PauseIcon } from "@heroicons/react/24/solid";
 
 const opts = {
   height: "300",
@@ -15,6 +16,8 @@ const opts = {
 
 export default function Home() {
   const [player, setPlayer] = useState<YT.Player | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
   const [currentTimeStamp, setCurrentTimeStamp] = useState<number>(0);
   const [currentLine, setCurrentLine] = useState<Line>();
@@ -53,24 +56,28 @@ export default function Home() {
 
   const handleToggle = () => {
     if (!player) return;
-
-    if (player.getPlayerState() === 1) {
+    if (isPlaying) {
       player.pauseVideo();
     } else {
       player.playVideo();
     }
   };
-  console.log(player.getPlayerState());
   // keep this console.log for adjust the time
   // console.log("time", currentTimeStamp);
   return (
-    <main className="flex min-h-screen min-w-dvw flex-col max-h-dvh items-center p-3 md:p-0 overflow-y-hidden">
+    <main className="flex min-h-dvh min-w-dvw flex-col max-h-dvh items-center p-3 md:p-0 overflow-y-hidden">
       <Youtube
         onReady={(e: YT.PlayerEvent) => {
           setPlayer(e.target);
         }}
         onPlay={() => {
-          if (!player) return;
+          if (!player) {
+            console.warn("player is not ready");
+            return;
+          }
+
+          setIsPlaying(true);
+
           const id = setInterval(() => {
             setCurrentTimeStamp(player?.getCurrentTime());
           }, 100);
@@ -78,14 +85,15 @@ export default function Home() {
           setIntervalId(id);
         }}
         onPause={() => {
+          setIsPlaying(false);
           if (!intervalId) {
             console.warn("intervalId is null");
             return;
           }
-
           clearInterval(intervalId);
         }}
         onEnd={() => {
+          setIsPlaying(false);
           if (!intervalId) {
             console.warn("intervalId is null");
             return;
@@ -98,7 +106,7 @@ export default function Home() {
         opts={opts}
       />
 
-      <section className="" id="metadata">
+      <section className="pb-3" id="metadata">
         <h1>{lyric.title}</h1>
         <p>{lyric.artist}</p>
         <p>作詞家: {lyric.lyricist}</p>
@@ -106,12 +114,12 @@ export default function Home() {
       </section>
 
       <div className="overflow-y-auto min-w-dvw">
-        <ul ref={lyricsContianerRef} className="">
+        <ul ref={lyricsContianerRef} className="flex flex-col gap-y-5">
           {lyric.lines.map((line) => {
             return (
               <li
                 id={`line-${line.timeStamp}`}
-                className={`transition-all leading-[3.5rem] duration-300 ${
+                className={`transition-all duration-300 ${
                   currentLine?.timeStamp === line.timeStamp
                     ? "text-white font-bold text-2xl"
                     : "text-white/70 text-xl"
@@ -137,8 +145,14 @@ export default function Home() {
       </div>
 
       <section className="fixed bottom-3 w-full px-3 md:px-0 flex justify-center items-center">
-        <nav className="flex items-center justify-center w-full px-16 py-2 border rounded-full shadow-xl gap-x-7 border-primary/70 backdrop-blur-md bg-white/30">
-          <button onMouseDown={handleToggle}>play</button>
+        <nav className="flex items-center justify-center w-full px-16 py-1 border rounded-full shadow-xl gap-x-7 border-white/20 backdrop-blur-md bg-white/10">
+          <button onMouseDown={handleToggle}>
+            {isPlaying ? (
+              <PauseIcon className="size-12" />
+            ) : (
+              <PlayIcon className="size-12" />
+            )}
+          </button>
         </nav>
       </section>
     </main>
