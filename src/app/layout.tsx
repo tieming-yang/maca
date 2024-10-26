904b4e360a4c3fbcf00c49aafac30a756f983879
Snow Yáng <ytm199891@gmail.com>
2024-10-26 14:08:35 +0800

    Polish style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,12 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Link
-          className="fixed z-50 top-4 left-7 font-mono text-xl font-semibold hidden sm:block"
-          href="/"
-        >
-         maca 
-        </Link>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul>
+    <main className="flex min-h-screen flex-col items-center py-24 space-y-12 px-5 md:px-0">
+      <Link
+        className="fixed z-30 top-4 left-7 font-mono text-xl font-semibold"
+        href="/"
+      >
+        maca
+      </Link>
+      <h1 className="text-3xl md:text-5xl xl:text-7xl">
+        Learn Japanese by{" "}
+        <span className="underline underline-offset-4 text-teal-500">
+          singing
+        </span>
+      </h1>
+      <ul className="flex flex-col">
         {Object.values(Song.songs).map((song) => {
           return (
             <li
