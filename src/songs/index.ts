d578b8dffc6d9ca2ffcdba9c83711871a0fd6c6a
Snow Yáng <ytm199891@gmail.com>
2025-01-19 21:08:57 -0600

    Remove dead code

--- src/songs/index.ts
+++ src/songs/index.ts
@@ -2,4 +2,3 @@ export { default as 大阪LOVER } from "./大阪LOVER";
 export { default as 欲望に満ちた青年団 } from "./欲望に満ちた青年団";
 export { default as 明日への手紙 } from "./明日への手紙";
 // export { default as キミシダイ列車 } from "./キミシダイ列車";
-export { SongName } from './SongName';
