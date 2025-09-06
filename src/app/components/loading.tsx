6413e1175e05d1f5495a473e0b405726636d0537
Snow Yáng <ytm199891@gmail.com>
2025-09-06 14:45:10 -0500

    Add loading component with fullscreen support

---
+++ src/app/components/loading.tsx
@@ -0,0 +1,17 @@
+import { LoaderCircle } from "lucide-react";
+
+export default function Loading({
+  isFullScreen = false,
+}: {
+  isFullScreen?: boolean;
+}) {
+  return isFullScreen ? (
+    <div className="fixed z-50 flex justify-center items-center top-0 w-dvh h-dvw bg-black/50 backdrop-blur-xs">
+      <LoaderCircle className="h-9 w-9 animate-spin" />
+    </div>
+  ) : (
+    <div className="flex justify-center items-center w-full h-full">
+      <LoaderCircle className="h-9 w-9 animate-spin" />
+    </div>
+  );
+}
