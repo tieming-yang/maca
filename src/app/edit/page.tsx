22993f5b1c1f7b1910243e12d23edea0dd9e5ad9
Snow Yáng <ytm199891@gmail.com>
2025-09-30 20:22:56 -0500

    Remove debug logging from EditPage function

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation";
 
 export default async function EditPage() {
   const isAdmain = await Profile.isAdmain();
-  console.log({ isAdmain });
   if (!isAdmain) {
     redirect("/");
   }
