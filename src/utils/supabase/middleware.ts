bf3cc225af398fefedb0ca3db70f9bde6212e7b8
Snow Yáng <ytm199891@gmail.com>
2025-10-14 21:05:54 -0500

    Restrict user redirection to login page only from profile path in updateSession middleware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -42,9 +42,7 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/error")
+    request.nextUrl.pathname.startsWith("/profile")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
