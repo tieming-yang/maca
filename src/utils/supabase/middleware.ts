925da91f571e9420009763b6255e69b15f57713e
Snow Yáng <ytm199891@gmail.com>
2025-10-18 10:37:32 -0500

    Fix /auth redirect after middleware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -51,6 +51,12 @@ export async function updateSession(request: NextRequest) {
     return NextResponse.redirect(url);
   }
 
+  if (user && request.nextUrl.pathname.startsWith("/auth")) {
+    const url = request.nextUrl.clone();
+    url.pathname = `/profile/${user.id}`;
+    return NextResponse.redirect(url);
+  }
+
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:
   // 1. Pass the request in it, like so:
