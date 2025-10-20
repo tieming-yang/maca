730f0699e9026f4b6cad7d5f9f29794ccab5d294
Snow Yáng <ytm199891@gmail.com>
2025-10-20 17:15:07 -0500

    Add user preference install pwa prompt presistent

--- src/app/components/install-pwa-prompt.tsx
+++ src/app/components/install-pwa-prompt.tsx
@@ -6,6 +6,13 @@ import { toast } from "sonner";
 export default function InstallPWAPrompt() {
   const [isIOS, setIsIOS] = useState(false);
   const [isStandalone, setIsStandalone] = useState(false);
+  const [isDisabled, _setIsDisabled] = useState(() => {
+    try {
+      return localStorage.getItem("isInstallPWAPromptDisabled") ?? false;
+    } catch (error) {
+      // we don't care
+    }
+  });
 
   useEffect(() => {
     const isIosUA = /iPad|iPhone|iPod/.test(navigator.userAgent);
@@ -19,13 +26,14 @@ export default function InstallPWAPrompt() {
     return null; // Don't show install button if already installed
   }
 
-  if (isIOS) {
+  if (isIOS && !Boolean(isDisabled)) {
     toast("To install this app on your iOS device, tap the share button", {
       description: 'and then "Add to Home Screen"',
       action: {
         label: "Got it",
         onClick: (e) => {
           e.preventDefault();
+          localStorage.setItem("isInstallPWAPromptDisabled", "true");
           toast.dismiss();
         },
       },
