63ce9c8fe9b298ed21429d076ab16c65e20cf6c1
Snow Yáng <ytm199891@gmail.com>
2025-10-20 20:46:15 -0500

    Add /vote

---
+++ src/app/vote/client-vote-page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ClientVotePage() {
+  return (
+    <main>
+      <h1>Vote for the song you want to learn</h1>
+
+    </main>
+  );
+}
---
+++ src/app/vote/page.tsx
@@ -0,0 +1,6 @@
+import ClientVotePage from "./client-vote-page";
+
+export default function VotePage() {
+  return <ClientVotePage />;
+}
+
