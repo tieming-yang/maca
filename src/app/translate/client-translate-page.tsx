ba2464e00ff6a3cc4f68214858c418c267bf7416
Snow Yáng <ytm199891@gmail.com>
2025-10-23 22:03:27 -0500

    feat: add /translate

---
+++ src/app/translate/client-translate-page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ClientTranslatePage() {
+  return <h1>Translate it to your flavor</h1>;
+}
---
+++ src/app/translate/page.tsx
@@ -0,0 +1,5 @@
+import ClientTranslatePage from "./client-translate-page";
+
+export default function TranslatePage() {
+  return <ClientTranslatePage />;
+}
