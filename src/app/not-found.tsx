5b02b8fe649921af772f1b6cfe16041cc3d01c21
Snow Yáng <ytm199891@gmail.com>
2025-11-06 09:02:58 -0600

    feat: add not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function RootNotFound() {
+  return (
+    <main className="flex justify-center-safe items-center-safe h-svh">
+      <h1 className="text-3xl">We don&apos;t have this page </h1>
+    </main>
+  );
+}
