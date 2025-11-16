a9af357dd4375f15cdc09590d3ac87b31a7fc1b5
Snow Yáng <ytm199891@gmail.com>
2025-11-16 13:09:25 -0600

    fix: add comments

--- src/utils/deepStrictEqual.ts
+++ src/utils/deepStrictEqual.ts
@@ -1,14 +1,27 @@
+/**
+ * @todo
+ * Add support to Map, Set, Error, RegExp
+ */
+
 export function deepStrictEqual<T>(actual: T, expect: T): boolean {
   // primitives
   if (Object.is(actual, expect)) {
     return true;
   }
-
+  if (
+    actual === null ||
+    expect === null ||
+    actual === undefined ||
+    expect === undefined ||
+    typeof actual !== typeof expect
+  ) {
+    return false;
+  }
   // Array
   const isActualArray = Array.isArray(actual);
   const isExpectArray = Array.isArray(expect);
-  if ((isActualArray && !isExpectArray) || (!isActualArray && isExpectArray))
-    return false;
+  if (isActualArray !== isExpectArray) return false;
+
   if (isActualArray && isExpectArray) {
     if (actual.length !== expect.length) return false;
 
@@ -30,34 +43,34 @@ export function deepStrictEqual<T>(actual: T, expect: T): boolean {
   const isActualObject = typeof actual === "object";
   const isExpectObject = typeof expect === "object";
   if (isActualObject && isExpectObject) {
-    if (
-      actual === null ||
-      expect === null ||
-      actual === undefined ||
-      expect === undefined ||
-      typeof actual !== typeof expect
-    ) {
+    /**
+     * @since
+     * const a = Object.create(null)
+     * const b = {}
+     * deepStrictEqual(a, b) -> true
+     */
+    if (Object.getPrototypeOf(actual) !== Object.getPrototypeOf(expect)) {
       return false;
-    } else {
-      const actualKeys = Object.keys(actual);
-      const expectKeys = Object.keys(expect);
-      if (actualKeys.length !== expectKeys.length) {
-        return false;
-      }
+    }
 
-      for (let i = 0; i < actualKeys.length; i++) {
-        const key = actualKeys[i];
-        if (Object.prototype.hasOwnProperty.call(expect, key)) {
-          if (!deepStrictEqual(actual[key], expect[key])) {
-            return false;
-          }
-        } else {
+    const actualKeys = Object.keys(actual);
+    const expectKeys = Object.keys(expect);
+    if (actualKeys.length !== expectKeys.length) {
+      return false;
+    }
+
+    for (let i = 0; i < actualKeys.length; i++) {
+      const key = actualKeys[i];
+      if (Object.prototype.hasOwnProperty.call(expect, key)) {
+        if (!deepStrictEqual(actual[key], expect[key])) {
           return false;
         }
+      } else {
+        return false;
       }
-
-      return true;
     }
+
+    return true;
   }
 
   return false;
