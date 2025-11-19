4c77b000ff3ee727aceb4485c7f867330bc44e5e
Snow Yáng <ytm199891@gmail.com>
2025-11-18 19:45:17 -0600

    fix: Type error: Type 'string | undefined' is not assignable to type 'string'.   Type 'undefined' is not assignable to type 'string'.

--- src/app/translate/[action]/[slug]/[translationVersionId]/page.tsx
+++ src/app/translate/[action]/[slug]/[translationVersionId]/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import { TranslationActionPageParam } from "../../page";
 import ClientTranslationUpdatePage from "./client-translation-update-page";
 
@@ -7,6 +8,10 @@ export default async function TranslationUpdatePage({
   params: TranslationActionPageParam;
 }) {
   const { action, slug, translationVersionId } = await params;
+  if (!translationVersionId) {
+    redirect(`/translate/create/${slug}`);
+  }
+
   return (
     <ClientTranslationUpdatePage
       action={action}
